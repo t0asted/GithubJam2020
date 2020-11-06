b8ac08e55c27e35430a39b42d24f4933bc947227
BeardyT <raziel.t.doom@hotmail.co.uk>
2020-11-06 17:23:54 +0000

    Added astronaut and got him moving
    Added the astronaut asset and removed the first person character controller. I aslo needed to add the other buttons in an or statement to ensure the animations would fire in all directions. Looking BOSS!

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -29,7 +29,7 @@ public class CharController : MonoBehaviour
 
     }
 
-    void Move()
+    public void Move()
     {
         Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"), 0, Input.GetAxis("VerticalKey"));
         Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
