27d9251be49a94b0b230332bb6a304b585acc0f3
hampton-matt <54047092+hampton-matt@users.noreply.github.com>
2020-11-17 23:51:15 +0000

    Gravity LUL

---
+++ Assets/Scripts/Controllers/S_GravityController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_GravityController : MonoBehaviour
+{
+    [SerializeField]
+    private S_Gravity Planet;
+
+    private Rigidbody Player;
+
+    [SerializeField]
+    private float RotateSpeed = 20;
+
+    [SerializeField]
+    private float Gravity = 20;
+
+    public void SetPlanet(S_Gravity planet)
+    {
+        Planet = planet;
+        Debug.Log("Planet value set to " + planet.gameObject.name);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Planet)
+        {
+            Vector3 gravityUp = (transform.position - Planet.transform.position).normalized;
+            Vector3 localUp = transform.up;
+
+            Debug.Log("Centre of Planet - " + Planet.transform.position);
+
+
+            Player.AddForce(-gravityUp * Gravity);
+
+            transform.rotation = Quaternion.FromToRotation(localUp, gravityUp) * transform.rotation;
+
+
+
+            //Vector3 gravityUp = Vector3.zero;
+            //
+            //gravityUp = (transform.position - gravityUp).normalized;
+            //
+            //Vector3 local = transform.up;
+            //
+            //Quaternion targetRotation = Quaternion.Euler(gravityUp);//Quaternion.FromToRotation(local, gravityUp) * transform.rotation;
+            //
+            //transform.rotation = targetRotation;
+            //
+            ////transform.up = Vector3.Lerp(transform.up, targetRotation, RotateSpeed * Time.deltaTime);
+            //
+            //Player.AddForce((-gravityUp * Gravity) * Player.mass);
+            //
+            //Debug.Log(targetRotation);
+            ////transform.Translate(-gravityUp);
+        }
+
+
+    }
+}
--- Assets/Scripts/Level/S_Gravity.cs
+++ Assets/Scripts/Level/S_Gravity.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+[RequireComponent(typeof(SphereCollider))]
 public class S_Gravity : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private SphereCollider sc;
+
+    [SerializeField]
+    private float TriggerRadius = 0.7f;
+
     void Start()
     {
-        
+        sc = this.GetComponent<SphereCollider>();
+        sc.isTrigger = true;
+        sc.radius = TriggerRadius;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        Debug.Log("Planet - " + other.gameObject.name + " Triggered Gravity");
+
+        if (other.GetComponent<S_GravityController>())
+        {
+            Debug.Log("Set Gravity Component Planet Value");
+
+            other.GetComponent<S_GravityController>().SetPlanet(this.GetComponent<S_Gravity>());
+        }
     }
 }
