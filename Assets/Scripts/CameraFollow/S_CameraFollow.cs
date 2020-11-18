e8688f30c45a8b8d1f08684ac960b5bffc4c6933
t0asted <t0asted@users.noreply.github.com>
2020-11-18 01:32:11 +0000

    Prefab change and camera follow

---
+++ Assets/Scripts/CameraFollow/S_CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ObjectToFollow;
+
+    public float speed = .5f;
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, ObjectToFollow.transform.position, speed);
+    }
+}
