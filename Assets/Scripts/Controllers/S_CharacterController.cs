e1c517f2e066135985b763222d75df3c78d4f7bd
hampton-matt <54047092+hampton-matt@users.noreply.github.com>
2020-11-19 23:30:14 +0000

    Jump / Jetpack and Sprint

--- Assets/Scripts/Controllers/S_CharacterController.cs
+++ Assets/Scripts/Controllers/S_CharacterController.cs
@@ -6,22 +6,30 @@ public class S_CharacterController : MonoBehaviour
 {
 
     [SerializeField]
-    private float m_Player_Movement_Speed = 2;
+    private float m_Player_Movement_Speed = 2f;
     [SerializeField]
-    private float m_Mouse_Sensititivity = 1;
+    private float m_Player_Sprint_Speed = 10f;
+    [SerializeField]
+    private float m_Jump_Force = 100f;
+    [SerializeField]
+    private float m_Mouse_Sensititivity = 1f;
     [SerializeField]
     private Transform m_Camera;
     [SerializeField]
     private Transform m_Target;
+    [SerializeField]
+    private LayerMask m_Ground_Mask;
 
 
 
     private Animator Animate;
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
         Animate = gameObject.GetComponentInChildren<Animator>();
+        rb = gameObject.GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
@@ -40,9 +48,34 @@ public class S_CharacterController : MonoBehaviour
         float forwards = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
 
-        Vector3 playerMovement = new Vector3(horizontal, 0f, forwards) * m_Player_Movement_Speed * Time.deltaTime;
 
-        transform.Translate(playerMovement, Space.Self);
+        if (Input.GetButton("Sprint"))
+        {
+            Vector3 playerMovement = new Vector3(horizontal, 0f, forwards) * m_Player_Sprint_Speed * Time.deltaTime;
+            transform.Translate(playerMovement, Space.Self);
+        } 
+        else
+        {
+            Vector3 playerMovement = new Vector3(horizontal, 0f, forwards) * m_Player_Movement_Speed * Time.deltaTime;
+            transform.Translate(playerMovement, Space.Self);
+        }
+
+        if (Input.GetButton("Jump") /*&& IsGrounded()*/)
+        {
+            rb.AddForce(transform.up * m_Jump_Force);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, -transform.up, out hit, 1, m_Ground_Mask)) {
+            return true;
+        }
+        return false;
+
     }
 
     private void Animation()
@@ -65,11 +98,11 @@ public class S_CharacterController : MonoBehaviour
         mouseX += Input.GetAxis("Mouse X") * m_Mouse_Sensititivity;
         mouseY -= Input.GetAxis("Mouse Y") * m_Mouse_Sensititivity;
 
-        m_Camera.transform.LookAt(m_Target);
+        //m_Camera.transform.LookAt(m_Target);
 
 
-        transform.localRotation = Quaternion.Euler(0, mouseX, 0) * transform.localRotation;
-        m_Target.localRotation = Quaternion.Euler(mouseY, mouseX, 0) * m_Target.localRotation;
+        transform.rotation = Quaternion.Euler(0, mouseX, 0) * transform.rotation;
+        //m_Target.localRotation = Quaternion.Euler(mouseY, mouseX, 0) * m_Target.localRotation;
 
     }
 
--- Assets/Scripts/Controllers/S_GravityController.cs
+++ Assets/Scripts/Controllers/S_GravityController.cs
@@ -13,7 +13,7 @@ public class S_GravityController : MonoBehaviour
     private float RotateSpeed = 20;
 
     [SerializeField]
-    private float Gravity = 20;
+    private float Gravity = 7;
 
     public void SetPlanet(S_Gravity planet)
     {
@@ -35,10 +35,12 @@ public class S_GravityController : MonoBehaviour
             Vector3 gravityUp = (transform.position - Planet.transform.position).normalized;
             Vector3 localUp = transform.up;
 
+            Quaternion target = Quaternion.FromToRotation(localUp, gravityUp) * transform.rotation;
 
+            transform.up = Vector3.Lerp(transform.up, gravityUp, RotateSpeed * Time.deltaTime);
+
+            //transform.rotation = Quaternion.FromToRotation(localUp, gravityUp) * transform.rotation;
             Player.AddForce(-gravityUp * Gravity);
-
-            transform.rotation = Quaternion.FromToRotation(localUp, gravityUp) * transform.rotation;
         }
 
 
