6a5134e00058d2c858983a22d0c5becc21c0c3c4
BeardyT <raziel.t.doom@hotmail.co.uk>
2020-11-26 19:13:28 +0000

    meshy mesh
    Added mesh to the mesh collider

--- Assets/Scripts/Planet/Planet.cs
+++ Assets/Scripts/Planet/Planet.cs
@@ -39,6 +39,8 @@ public class Planet : MonoBehaviour
         terrainFaces = new TerrainFace[6];
 
         Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
+        
+        
 
         for (int i = 0; i < 6; i++)
         {
@@ -46,12 +48,12 @@ public class Planet : MonoBehaviour
             {
                 GameObject meshObj = new GameObject("mesh");
                 meshObj.transform.parent = transform;
-
+                Mesh mesh = new Mesh();
                 meshObj.AddComponent<MeshRenderer>();
-                meshObj.AddComponent<MeshCollider>();
-                
+                MeshCollider meshcollider = meshObj.AddComponent<MeshCollider>();
+                meshcollider.sharedMesh = mesh;
                 meshFilters[i] = meshObj.AddComponent<MeshFilter>();
-                meshFilters[i].sharedMesh = new Mesh();
+                meshFilters[i].sharedMesh = mesh;
 
             }
 
