6f2b4cfd3f48b3f9b349e41c8a8a99454a70ec76
BeardyT <raziel.t.doom@hotmail.co.uk>
2020-11-28 16:17:04 +0000

    light moving and skybox

---
+++ Assets/Scripts/LightSource.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSource : MonoBehaviour
+{
+    private float timestamp;
+
+    void Start()
+    {
+        timestamp = Time.time;
+    }
+
+    void Update()
+    {
+        float angle = (Time.time - timestamp) / 1140.0f * 360.0f;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
