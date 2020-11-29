471062483681bece203524c165b6ff23288eea59
hampton-matt <54047092+hampton-matt@users.noreply.github.com>
2020-11-29 22:32:47 +0000

    Updated resource Enums

--- Assets/Scripts/Data/Enums/Item/Enum_Items.cs
+++ Assets/Scripts/Data/Enums/Item/Enum_Items.cs
@@ -1,25 +1,53 @@
 ﻿[System.Serializable]
 public enum Enum_Items
 {
+    // Resource
     None = 0,
     Aluminium = 1,
     Iron = 2,
-    Titanium = 3,
+    Copper = 3,
+    Titanium = 4,
+    Gold = 5,
+    Silicon = 6,
+    Oxygen = 7,
+    Carbon = 8,
+    Hydrogen = 9,
+    Fluorine = 10,
+    Helium = 11,
 
-    Junk = 50,
+    // Refined
+    Pure_Copper = 50,
+    Tritium = 51,
+    Bio_Plastic = 52,
+    
+    // Synthesised
+    Epoxy = 101,
+    Teflon = 102,
 
-    Electricity = 100,
+    // Parts
+    Ship_Plating = 201,
+    Canister = 202,
+    Silicon_Wafers = 203,
+    Circuit_Board = 204,
 
-    Upgrade_Pack_1 = 3000,
-    Upgrade_Pack_2 = 3001,
-    Upgrade_Pack_3 = 3002,
+    // Upgrades
+    Upgrade_Pack_1 = 500,
+    Upgrade_Pack_2 = 501,
 
+    // Machines
+    Machine_Rocket = 1000,
+    Machine_Miner = 1001,
+    Machine_Machine_Fabricator = 1002,
+    Machine_Extractor = 1003,
+    Machine_Refiner = 1004,
+    Machine_Biodome = 1005,
+    Machine_Upgrade_Fabricator = 1006,
+    Machine_Parts_Fabricator = 1007,
+    Machine_Relay = 1008,
+    Machine_Synthesiser = 1009,
+    Machine_Storage = 1010,
 
-    Machine_Miner = 4000,
-    Machine_Crafter = 4001,
-    Machine_Storage = 4002,
-    Machine_Relay = 4003,
-    Machine_Solar = 4004,
-    Machine_Battery = 4005,
-    Machine_Rocket =4006
+    //Junk = 50,
+
+    //Electricity = 100,
 }
