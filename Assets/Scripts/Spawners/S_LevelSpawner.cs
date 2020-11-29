9823040a1b609f4f4dab1ccb86d4e1422f1d6f0f
hampton-matt <54047092+hampton-matt@users.noreply.github.com>
2020-11-29 03:19:51 +0000

    Random Planet Position Spawning
    Help me pls.....

--- Assets/Scripts/Spawners/S_LevelSpawner.cs
+++ Assets/Scripts/Spawners/S_LevelSpawner.cs
@@ -5,7 +5,18 @@ using UnityEngine;
 public class S_LevelSpawner : MonoBehaviour
 {
     [SerializeField]
-    private GameObject m_astroid;
+    private GameObject m_Astroid;
+
+    [SerializeField]
+    private GameObject m_AsteroidStarter;
+
+    [SerializeField]
+    private int m_MinimumPlanetColliderSeperation = 50;
+
+    [SerializeField]
+    private int m_MaximumPlanetColliderSeperation = 250;
+
+
 
     private S_GameController ref_GameController;
     private List<SO_Planet> LevelData;
@@ -25,19 +36,73 @@ public class S_LevelSpawner : MonoBehaviour
     {
         LevelData = LevelToLoad;
 
-        foreach (var Astroid in LevelData)
+        // Sort planet data by planet size
+        List<SO_Planet> sortedList = new List<SO_Planet>(LevelToLoad);
+        sortedList.Sort((SO_Planet p1, SO_Planet p2) => p2.size.CompareTo(p1.size));
+
+        // Calculate planet seperation boundaries
+        int minPlanetSeperation = (3 * sortedList[0].size) + m_MinimumPlanetColliderSeperation;
+        Debug.Log(minPlanetSeperation);
+        int maxPlanetSeperation = minPlanetSeperation + m_MaximumPlanetColliderSeperation - m_MinimumPlanetColliderSeperation;
+
+        // Create spawned planet list and add first planet (STARTER)
+        List<Vector3> spawnedPlanets = new List<Vector3>();
+        spawnedPlanets.Add(new Vector3(0, 0, 0));
+
+        // Add planet to world and remove from list ready for for loop
+        InstantiatePlanet(m_AsteroidStarter, LevelData[0], new Vector3(100, 0, 0));
+        LevelData.Remove(LevelData[0]);
+
+
+        foreach (var Planet in LevelData)
         {
-            if (m_astroid != null)
+            bool spawnable = false;
+            Vector3 randomPosition = Vector3.zero;
+            while (!spawnable)
             {
-                GameObject astroidGO = Instantiate(m_astroid, this.transform);
-                astroidGO.GetComponent<S_Astroid>().SetSizeOfAstroid(Astroid);
-                astroidGO.transform.SetParent(this.transform);
-                
+                // Randomly choose planet to spawn from
+                int closestPlanet = Random.Range(0, spawnedPlanets.Count);
+                int largestVector = Random.Range(0, 3);
+
+                int[] randomValues = new int[] {
+                    Random.Range(0, minPlanetSeperation),
+                    Random.Range(0, minPlanetSeperation),
+                    Random.Range(0, minPlanetSeperation)
+                };
+
+                randomValues[largestVector] = Random.Range(minPlanetSeperation + 1, maxPlanetSeperation);
+
+                randomPosition = spawnedPlanets[closestPlanet] + new Vector3(randomValues[0], randomValues[1], randomValues[2]);
+
+                // Check position against positions of spawned planets
+                foreach (Vector3 spawnedVector in spawnedPlanets)
+                {
+                    if (Vector3.Distance(randomPosition, spawnedVector) > minPlanetSeperation)
+                    {
+                        spawnable = true;
+                    } 
+                    else
+                    {
+                        spawnable = false;
+                        break;
+                    }
+                }
             }
+            InstantiatePlanet(m_Astroid, Planet, randomPosition);
+            spawnedPlanets.Add(randomPosition);
         }
 
         return true;
     }
 
-   
+    // Function to add planet to game world
+    private void InstantiatePlanet(GameObject asteroid, SO_Planet data, Vector3 position)
+    {
+        // Check gameobject is not null
+        if (asteroid)
+        {
+            GameObject toSpawn = Instantiate(asteroid, position, new Quaternion(), this.transform);
+            toSpawn.GetComponent<S_Astroid>().SetSizeOfAstroid(data);
+        }
+    }
 }
