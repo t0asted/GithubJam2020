389046043f9504bb88ea45a7ddc8bb902f02dc67
t0asted <t0asted@users.noreply.github.com>
2020-11-30 00:09:10 +0000

    Machine, Repair lists

---
+++ Assets/Scripts/Data/Resources/SO_ListMachine.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Item/Machine List")]
+public class SO_ListMachine : ScriptableObject
+{
+    public List<MachineList> ItemList = new List<MachineList>();
+}
+
+[System.Serializable]
+public class MachineList
+{
+    public SO_Machine ItemData;
+}
