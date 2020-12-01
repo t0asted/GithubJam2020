b532305a247dd2547e71b68583a2756c0c173112
hampton-matt <54047092+hampton-matt@users.noreply.github.com>
2020-12-01 21:24:58 +0000

    Finito

--- Assets/Scripts/UI/S_Hover.cs
+++ Assets/Scripts/UI/S_Hover.cs
@@ -11,21 +11,17 @@ public class S_Hover : MonoBehaviour
     private Color m_Default_Color = new Color(188,188,188,255);
     [SerializeField]
     private Color m_Hover_Color = new Color(255, 255, 255, 255);
-
+    [SerializeField]
     private TextMeshProUGUI buttonText;
 
-    void Start()
-    {
-        buttonText = GetComponentInChildren<TextMeshProUGUI> ();
-    }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        buttonText.color = m_Hover_Color;
+        if (buttonText) buttonText.color = m_Hover_Color;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        buttonText.color = m_Default_Color;
+        if (buttonText) buttonText.color = m_Default_Color;
     }
 }
